9eeaecff502cc8be4867722aae35e8c7c59c8532
Nolan Beard <nolan.beard@eure.jp>
2021-04-11 23:38:31 +0900

    oops

--- brew.sh
+++ brew.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Install xcode cli tools
-xcode-select --install
-
-# Install command-line tools using Homebrew.
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 # Make sure we’re using the latest Homebrew.
 brew update
 
